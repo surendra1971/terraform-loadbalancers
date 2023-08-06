9246bbf82c69d81aaeb3db6ab9b3e0b8632910e6
CodingManoj <mvp2612@hotmail.com>
2023-08-05 21:48:02 -0400

    Git Auto Push

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 module "alb-public" {
   source                      = "./vendor/modules/alb"
   ENV                         = var.ENV
-  ALB_NAME                    = "roboshop-public-alb"
   INTERNAL                    = false 
   ALB_NAME                    = "robot-public-alb"
 }
@@ -9,7 +8,6 @@ module "alb-public" {
 module "alb-private" {
   source                      = "./vendor/modules/alb"
   ENV                         = var.ENV
-  ALB_NAME                    = "roboshop-private-alb"
   INTERNAL                    = true 
   ALB_NAME                    = "robot-private-alb"
 }
