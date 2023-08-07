0cd452b7b7d3ec434fd0adf055967969e4be6524
CodingManoj <mvp2612@hotmail.com>
2023-08-06 21:43:53 -0400

    Git Auto Push

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,15 @@ output "PRIVATE_ALB_ARN" {
 output "PRIVATE_LISTENER_ARN" {
     value   =  module.alb-private.LISTERNER_ARN[0]
 }
+
+output "PRIVATE_ALB_ADDRESS" {
+    value   =  module.alb-private.ALB_ADDRESS
+}
+
+output "PUBLIC_ALB_ADDRESS" {
+    value   =  module.alb-public.ALB_ADDRESS
+}
+
+
+
+
